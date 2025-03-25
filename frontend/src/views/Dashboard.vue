0b5de255b6e532543a0dd3bd24fbaee5c954f10a
Takeruso <102784225@student.swin.edu.au>
2025-03-25 22:10:08 +1100

    Add frontend Vue project with router and API setup

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container mt-5">
+      <h1>Dashboard</h1>
+      <button @click="fetchHello" class="btn btn-primary">Get Hello</button>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import api from '../api'
+  
+  const message = ref('')
+  
+  const fetchHello = async () => {
+    const res = await api.get('/api/hello')
+    message.value = res.data.message
+  }
+  </script>
+  
