cbfd5266165398b8176f7e3017e50be346a2e2a1
Takeruso <102784225@student.swin.edu.au>
2025-03-31 09:49:28 +1100

    Add ESLint + Prettier configuration for frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+      'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: ['vue'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'vue/multi-word-component-names': 'off',
+      'prettier/prettier': ['warn']
+    }
+  }
+  
