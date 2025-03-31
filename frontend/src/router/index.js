4dc5ea24a6f4e6b41090b0f8942e4d50eca14350
Takeruso <102784225@student.swin.edu.au>
2025-04-01 09:42:53 +1100

    chore: update vite to ^6.2.4 to resolve CVE-2025-31125

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import SamplePage from '../views/sample.vue'
+import AdminDashboard from '../views/AdminDashboard.vue'
+import FacilityManagement from '../views/FacilityManagement.vue'
+import ServiceManagement from '../views/ServiceManagement.vue'
+import Alerts from '../views/Alerts.vue'
 
 const routes = [
   { path: '/', component: Login },
   { path: '/dashboard', component: Dashboard },
-  { path: '/sample', component: SamplePage }
+  { path: '/sample', component: SamplePage },
+  { path: '/admin', component: AdminDashboard },
+  { path: '/admin/facility', component: FacilityManagement },
+  { path: '/admin/service', component: ServiceManagement },
+  { path: '/admin/alerts', component: Alerts }
 ]
 
 export default createRouter({
