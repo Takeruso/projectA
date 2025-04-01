fc5ae8adee70247b02a85e22e17be567ee8681d5
Takeruso <102784225@student.swin.edu.au>
2025-04-02 08:42:15 +1100

    WIP: router updated temporarily

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import SamplePage from '../views/sample.vue'
+import ResidentDashboard from '../views/ResidentDashboard.vue'
 
 const routes = [
   { path: '/', component: Login },
   { path: '/dashboard', component: Dashboard },
-  { path: '/sample', component: SamplePage }
+  { path: '/sample', component: SamplePage },
+  { path: '/residentDashboard', component: ResidentDashboard }
 ]
 
 export default createRouter({
