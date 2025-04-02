d2fd3eaa83386f4135f21b2f2825a9bc11934eff
William Fisher <104553333@student.swin.edu.au>
2025-04-02 18:27:29 +1100

    updating admin dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,9 +4,9 @@ import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import SamplePage from '../views/sample.vue'
 import AdminDashboard from '../views/AdminDashboard.vue'
-import FacilityManagement from '../views/FacilityManagement.vue'
-import ServiceManagement from '../views/ServiceManagement.vue'
-import Alerts from '../views/Alerts.vue'
+//import FacilityManagement from '../views/FacilityManagement.vue'
+//import ServiceManagement from '../views/ServiceManagement.vue'
+//import Alerts from '../views/Alerts.vue'
 
 const routes = [
   { path: '/', component: Login },
