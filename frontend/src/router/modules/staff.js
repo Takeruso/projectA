44ba3ee23e468629f9c3df0dec01d49e3b4b356a
bananaMatcha <ngochan21.nh@gmail.com>
2025-04-15 09:00:49 +1000

    update routes to staff pages

--- frontend/src/router/modules/staff.js
+++ frontend/src/router/modules/staff.js
@@ -1,6 +1,14 @@
 export default [
   {
     path: '/staff',
-    component: () => import('@/views/staff/StaffDashboard.vue')
+    component: () => import('@/views/staff/staffDashboard.vue')
+  },
+  {
+    path: '/staff/appointments',
+    component: () => import('@/views/staff/appointmentCarer.vue')
+  },
+  {
+    path: '/staff/residents',
+    component: () => import('@/views/staff/residentInfo.vue')
   }
 ]
