46b6e37369cb3acc4941dd7c5597c53b405a2d2c
Takeruso <102784225@student.swin.edu.au>
2025-04-29 12:41:49 +1000

    adding staff router front~

---
+++ frontend/src/router/modules/staff.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    path: '/staff',
+    component: () => import('@/views/staff/staffDashboard.vue')
+  },
+  {
+    path: '/staff/appointment',
+    component: () => import('@/views/staff/appointmentCarer.vue')
+  },
+  {
+    path: '/staff/residentInfo',
+    component: () => import('@/views/staff/residentInfo.vue')
+  },
+  {
+    path: '/staff/reports',
+    component: () => import('@/views/staff/staffMedicalReport.vue')
+  },
+  {
+    path: '/staff/medication',
+    component: () => import('@/views/staff/staffMedications.vue')
+  },
+  {
+    path: '/staff/facilityCheckup',
+    component: () => import('@/views/staff/facilityCheckup.vue')
+  }
+]
